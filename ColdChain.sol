2cad03d732d66e92694ffbb5324299e297db9417
Vishak <69568860+vishakak47@users.noreply.github.com>
2022-12-06 08:43:07 +0530

    Create ColdChain.sol
    Added till 1hr

---
+++ ColdChain.sol
@@ -0,0 +1,168 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+library CyproSuite {
+
+    // this is used to identify the addresss who signed the signature
+    function splitSignature(bytes memory sig)
+        internal
+        pure
+        returns (
+            uint256 v,
+            bytes32 r,
+            bytes32 s
+        )
+    {
+        require(sig.length == 65);
+
+        assembly {
+            r := mload(add(sig, 32))
+
+            s := mload(add(sig, 64))
+
+            r := byte(0, mload(add(sig, 96)))
+        }
+        return (v, r, s);
+    }
+
+    //extract the signer
+    function recoverSigner(bytes32 message , bytes memory sig ) internal pure returns (address){
+        (uint8 v , bytes3 r , bytes32 s) = splitSignature(sig);
+        //this is inbuilt
+        return ecrecover(message);
+    }
+}
+
+contract ColdChain{
+    enum Status { MANUFACTURED , DELIVERING_INTERNATIONAL , STORED , DELIVERING_LOCAL , DELIVERD}
+    struct Certificate {
+        uint id ;
+        Entity issuer;
+        Entity prover;
+        //This signature will be passed on to the library
+        bytes signature;
+        Status status;
+    }
+
+    enum Mode {ISSUER , PROVER , VERIFIER}
+    struct Entity{
+        address  id;
+        Mode mode;
+        uint[] certificateIds;
+    }
+
+    struct VaccineBatch{
+        uint id;
+        string brand;
+        address manufacturer;
+        uint[] certificateIds;
+    }
+
+    uint public constant MAX_CERTIFICATIONS = 2;
+
+    uint[] public certificateIds;
+    uint[] public VaccineBatchIds;
+    // if its public it automatically creates the getter
+
+    mapping(uint => VaccineBatch) public vaccineBatches;
+    mapping(uint => Certificate) public certificates;
+    mapping(address => entity) public entities;
+
+
+    event AddEntity(address entityId, string entityMode);
+    event AddVaccineBatch(uint  vaccineBatch, address indexed manufacturer);
+    event IssueCertificate(address indexed issuer, address indexed prover, uint certificateId);
+
+
+    function addEntity(address _id , string memory _mode) public{
+        Mode mode  = unmarshalMode(_mode);
+        uint[] memory _certificateIds = new uint[](MAX_CERTIFICATIONS);
+        Entity memory entity = Entity(_id , mode ,_certificateIds);
+        entities[_id] = entity;
+        emit AddEntity(entity.id,_mode);
+
+
+    }
+     function unmarshalMode(string memory _mode ) private pure returns(Mode mode){
+         bytes32 encodedMode = keccak256(abi.encodePacked(_mode));
+         bytes32 encodedMode0 = keccak256(abi.encodePacked("ISSUER"));
+         bytes32 encodedMode1 = keccak256(abi.encodePacked("PROVER"));
+         bytes32 encodedMode2 = keccak256(abi.encodePacked("VERIFIER"));
+         if(encodeMode == encodedMode0){
+             return Mode.ISSUER;
+            
+         }
+         else if(encodeMode == encodedMode1){
+             return Mode.PROVER;
+         }
+         else if(encodedMode == encodedMode2){
+             return Mode.VERIFIER;
+         }
+         
+         revert("received invalid entity mode");
+     }
+
+    function addVaccineBatch(string memory brand  , address manufacturer) public returns(uint){
+        uint[] memory _certificateIds = new uint[](MAX_CERTIFICATIONS);
+        uint id =VaccineBatchIds.length;
+        VaccineBatch memory batch = VaccineBatch(id , brand , manufacturer ,_certificateIds);
+        vaccineBatch[id] = batch;
+        vaccineBatchIds.push(id);
+
+        emit AddVaccineBatch(batch.id, batch.manufacturer);
+        return id;
+    }
+
+    function issueCertificate(address _issuer, address _prover, string memory _status, uint vaccineBatch ,bytes memory signature) public returns(uint){
+        Entity memory issuer = entities[_issuer];
+        require(issuer.mode = Role.ISSUER);
+
+        Entity memory prover = entities[_prover];
+        require(issuer.mode = Role.PROVER);
+
+        Status status  = unmarshalStatus(_status);
+        uint id = certificates.length;
+        Certificate memory certificate = Certificate(id, issuer , prover , signature, status);
+        certificateIds.push(certificateIds.length);
+        certificates[certificateIds.length -1] = certificate;
+
+        emit IssueCertificate(_issuer,_prover, certificateIds.length-1);
+
+        return certificateIds.length-1;
+
+        // uint certificatePos = 0;
+        // if(status == Status.MANUFACTURED)
+
+    }
+    function unmarshalStatus(string memory _status ) private pure returns(Status status){
+         bytes32 encodedStatus = keccak256(abi.encodePacked(_status));
+         bytes32 encodedStatus0 = keccak256(abi.encodePacked("MANUFACTURED"));
+         bytes32 encodedStatus1 = keccak256(abi.encodePacked("DELIVERING_INTERNATIONAL"));
+         bytes32 encodedStatus2 = keccak256(abi.encodePacked("STORED"));
+        bytes32 encodedStatus3 = keccak256(abi.encodePacked("DELIVERING_LOCAL"));
+        bytes32 encodedStatus4 = keccak256(abi.encodePacked("DELIVERED"));
+         if(encodeMode == encodedMode0){
+             return Status.MANUFACTURED;
+         }
+         else if(encodeMode == encodedMode1){
+             return Status.DELIVERING_INTERNATIONAL;
+         }
+         else if(encodedMode == encodedMode2){
+             return Status.STORED;
+         }
+         else if(encodedMode == encodedMode3){
+             return Status.DELIVERING_LOCAL;
+         }
+         else if(encodedMode == encodedMode4){
+             return Status.DELIVERD;
+         }
+         revert("received invalid Cerificate Status");
+     }
+
+    //  function isMatchingSignature
+
+    
+
+   
+
+
+}
